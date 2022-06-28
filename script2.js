d0100a8010efe46988bc5873630e72a748c3d04a
Carlos Magno <81724679+carlosmagno@users.noreply.github.com>
2022-06-27 22:15:44 -0300

    Included References

--- script2.js
+++ script2.js
@@ -1,5 +1,22 @@
 
 
+// function menuPrincipal(){
+       
+// }
+
+// function mostrarMenu(){
+//     alert("olá")
+// }
+
+// window.onload = function(){
+//     // var iconMenu = document.getElementById("icon_menu")
+//     if(iconMenu){
+//         iconMenu.addEventListener("click", (event) => {
+//             mostrarMenu()
+//         });
+//     }
+
+// }
 
 
 
